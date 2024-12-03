d18e98d3de44ea795667a478a9b1e63ccdb7e596
Dasadno <Yakaleks96@gmail.com>
2024-12-03 14:36:09 +0500

    fucking wrong letter, i hate it so much

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ type Response struct {
 var db *gorm.DB
 
 func initDB() {
-	dsn := "host=localhost user=postgres password=yourpassword dbname=postgres port=5432 sslmode=disabled"
+	dsn := "host=localhost user=postgres password=yourpassword dbname=postgres port=5432 sslmode=disable"
 	var err error
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
